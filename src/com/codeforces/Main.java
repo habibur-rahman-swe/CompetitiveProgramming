901edf7e4e41ea0aac0e61a214b6ea28fb28ed18
habibur-rahman-swe <habibur0199mhr@gmail.com>
2023-12-21 00:07:58 +0600

    TD2123

--- src/com/codeforces/Main.java
+++ src/com/codeforces/Main.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.stream.Collectors;
 
 public class Main {
 
@@ -26,13 +24,33 @@ public class Main {
 			reader = new BufferedReader(new InputStreamReader(System.in));
 		}
 
-//		int testCases = readInteger();
-		int testCases = 1;
+		int testCases = readInteger();
+//		int testCases = 1;
 
 //		long startTime = System.nanoTime();
 
 		for (int testCase = 1; testCase <= testCases; testCase++) {
-			
+
+			int n = readInteger();
+
+			String[] str = new String[n];
+
+			for (int i = 0; i < n; i++) {
+				str[i] = readString();
+			}
+
+			int[][] arr = new int[n][6];
+
+			fillArray(arr, str);
+
+			int ans = 0;
+
+			for (int i = 0; i < 5; i++) {
+				ans = Math.max(ans, subAns(arr, i));
+			}
+
+			sb.append(ans);
+
 			sb.append("\n");
 		}
 
@@ -40,7 +58,35 @@ public class Main {
 		writer.flush();
 	}
 
-	
+	private static int subAns(int[][] arr, int x) {
+		int[] ans = new int[arr.length];
+		for (int i = 0; i < ans.length; i++) {
+			ans[i] = 2 * arr[i][x] - arr[i][5];
+		}
+		
+		int res = 0, idx = ans.length - 1;
+		
+		Arrays.sort(ans);
+		
+		while (idx >= 0 && res + ans[idx] > 0) {
+			res += ans[idx];
+			idx--;
+		}
+		
+		return ans.length - idx - 1;
+	}
+
+	private static void fillArray(int[][] arr, String[] str) {
+		for (int i = 0; i < arr.length; i++) {
+			for (char c : str[i].toCharArray()) {
+				arr[i][c - 'a']++;
+			}
+			for (int j = 0; j < 5; j++) {
+				arr[i][5] += arr[i][j];
+			}
+		}
+	}
+
 	// ------------------------------------------------------------------------------------
 	private static String readString() throws IOException {
 		String s = reader.readLine();
